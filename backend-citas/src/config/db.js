d7434e424f23ce5838158cafc577d48e2e891441
Sam-cmd-star <darkkrisalix616@gmail.com>
2026-06-02 14:58:33 -0500

    "Configuración de MongoDB Atlas"

--- backend-citas/src/config/db.js
+++ backend-citas/src/config/db.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const conectarDB = async () => {
   try {
     const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/sistema_citas';
-    await mongoose.connect(MONGO_URI);
-    console.log('🍃 Conectado exitosamente a MongoDB Local');
+    const urlConexion = process.env.MONGO_URI;
+    
+    await mongoose.connect(urlConexion);
+    console.log('🚀 ¡Conectado con éxito a MongoDB Atlas en la nube!');
   } catch (error) {
-    console.error('❌ Error de conexión a MongoDB:', error);
-    process.exit(1);
+    console.error('❌ Error al conectar a MongoDB:', error.message);
+    process.exit(1); // Detiene el servidor si falla la conexión
   }
 };
+   
 
 module.exports = conectarDB;
